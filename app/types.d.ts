41a211e4d269b82f131810ec95d220b87daee73d
9ightshade <aniekemeusanga@gmail.com>
2025-02-21 23:25:14 +0100

    clean up

--- app/types.d.ts
+++
@@ -1,9 +0,0 @@
-
-interface MessageType {
-    id: number;
-    text: string;
-    timestamp: string;
-    detectedLanguage: string | null;
-    summary: string | null;
-    translation: string | null;
-}
